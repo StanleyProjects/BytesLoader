21ec39316b697277bb816986340fb79ff4feedaa
Stanley Wintergreen <kepocnhh@protonmail.com>
2024-11-05 15:42:50 +0300

    init unit test

---
+++ lib/src/main/kotlin/sp/kx/bytes/loader/BytesLoader.kt
@@ -0,0 +1,3 @@
+package sp.kx.bytes.loader
+
+class BytesLoader
---
+++ lib/src/test/kotlin/sp/kx/bytes/loader/BytesLoaderTest.kt
@@ -0,0 +1,10 @@
+package sp.kx.bytes.loader
+
+import org.junit.jupiter.api.Test
+
+internal class BytesLoaderTest {
+    @Test
+    fun loadTest() {
+        TODO("BytesLoaderTest:loadTest")
+    }
+}
